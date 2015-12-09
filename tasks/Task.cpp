d6ad23f2c35f2de5c2c3fd2131fcd944dd4fd031
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2015-12-09 13:00:17 +0100

    Calibare color frames with depth images, syncronize the output

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -4,32 +4,39 @@
 #include <base/Logging.hpp>
 #include <GL/freeglut.h>
 #include <GLFW/glfw3.h>
+#include <libfreenect2/registration.h>
 
 using namespace kinect2;
 using namespace libfreenect2;
 
 Task::Task(std::string const& name)
     : TaskBase(name)
+    , undistorted(512, 424, 4)
+    , registered(512, 424, 4)
 {
- glfwInit();
- driver = new Freenect2();
- color_frame = 0;
- depth_frame = 0;
- ir_frame = 0;
+    glfwInit();
+    driver = new Freenect2();
+    color_frame[0] = 0;
+    color_frame[1] = 0;
+    depth_frame = 0;
+    ir_frame = 0;
+    depth = 0;
+    ir = 0;
+    rgb = 0;
 }
 
 Task::Task(std::string const& name, RTT::ExecutionEngine* engine)
     : TaskBase(name, engine)
+    , undistorted(512, 424, 4)
+    , registered(512, 424, 4)
 {
- glfwInit();
- driver = new Freenect2();
+    glfwInit();
+    driver = new Freenect2();
 }
 
 Task::~Task()
 {
 }
-
-
 
 /// The following lines are template definitions for the various state machine
 // hooks defined by Orocos::RTT. See Task.hpp for more detailed
@@ -37,28 +44,27 @@ Task::~Task()
 
 bool Task::configureHook()
 {
-    if (! TaskBase::configureHook())
-        return false;
+    if (!TaskBase::configureHook()) return false;
     /*
     int argc=0;
     char **argv=0;
-    glutInit(&argc,argv);   
+    glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_RGBA);
     glutCreateWindow("test");
     */
 
     int device_count = driver->enumerateDevices();
-    if(device_count < 1){
-        LOG_FATAL_S << "Could not open Kinect"; 
-        return false; 
+    if (device_count < 1) {
+        LOG_FATAL_S << "Could not open Kinect";
+        return false;
     }
 
-//    device = driver->openDefaultDevice();
-    device = driver->openDevice(0); //TODO handle more than 1 device
-    if(!device){
-        LOG_FATAL_S << "Could not open Kinect"; 
+    //    device = driver->openDefaultDevice();
+    device = driver->openDevice(0);  // TODO handle more than 1 device
+    if (!device) {
+        LOG_FATAL_S << "Could not open Kinect";
         return false;
-    }else{
+    } else {
         LOG_DEBUG_S << "Device opened successfull";
     }
     device->setIrAndDepthFrameListener(this);
@@ -67,10 +73,10 @@ bool Task::configureHook()
 }
 bool Task::startHook()
 {
-    if (! TaskBase::startHook())
-        return false;
+    if (!TaskBase::startHook()) return false;
 
     device->start();
+    registration = new libfreenect2::Registration(device->getIrCameraParams(), device->getColorCameraParams());
     return true;
 }
 void Task::updateHook()
@@ -85,97 +91,131 @@ void Task::stopHook()
 {
     TaskBase::stopHook();
     device->stop();
+    delete registration;
 }
 void Task::cleanupHook()
 {
     TaskBase::cleanupHook();
     delete device;
 }
-        
-bool Task::onNewFrame(Frame::Type type, Frame *frame){
-    size_t &width = frame->width;
-    size_t &height = frame->height;
+
+bool Task::writeColorFrame(libfreenect2::Frame* frame, bool orginal)
+{
+    size_t& width = frame->width;
+    size_t& height = frame->height;
     size_t bpp = frame->bytes_per_pixel;
     unsigned char* data = frame->data;
 
-    if(type == Frame::Color){
-        //The kinect guy desicded to use BGRX (with alpha channel and alpha always zero so we handle alpha as non-existend in the following)
-        if(bpp == 4 || bpp == 3){
-            size_t bpp_internal = 3;
+    // The kinect guy desicded to use BGRX (with alpha channel and alpha always zero so we handle alpha as non-existend in the following)
+    if (bpp == 4 || bpp == 3) {
+        size_t bpp_internal = 3;
 
-            if(!color_frame){
-                color_frame = new base::samples::frame::Frame(width,height,8,base::samples::frame::MODE_RGB);
-            }
-
-            color_frame->time = base::Time::now();
-            char data_new[width*height*bpp_internal];
-            for(size_t x=0;x<width;x++){
-                for(size_t y=0;y<height;y++){
-                    data_new[0 + x*bpp_internal + y*bpp_internal*width] =  data[2 + (width-x)*bpp + y*bpp*width];
-                    data_new[1 + x*bpp_internal + y*bpp_internal*width] =  data[1 + (width-x)*bpp + y*bpp*width];
-                    data_new[2 + x*bpp_internal + y*bpp_internal*width] =  data[0 + (width-x)*bpp + y*bpp*width];
-                }
-            }
-            color_frame->setImage(data_new,width*height*bpp_internal);
-            color_frame_p.reset(color_frame);
-            _color_frame.write(color_frame_p);
-        }else{
-            LOG_ERROR_S << "Color Image: Unssuported bits per pixel size of " << bpp;
-            delete frame;
-            return false;
+        if (!color_frame[orginal]) {
+            color_frame[orginal] = new base::samples::frame::Frame(width, height, 8, base::samples::frame::MODE_RGB);
         }
-    }else if(type == Frame::Ir){
-        if(bpp == 4){
-            if(!ir_frame){
-                ir_frame = new base::samples::frame::Frame(width,height,32,base::samples::frame::MODE_GRAYSCALE);
+
+        color_frame[orginal]->time = base::Time::now();
+        char data_new[width * height * bpp_internal];
+        for (size_t x = 0; x < width; x++) {
+            for (size_t y = 0; y < height; y++) {
+                data_new[0 + x * bpp_internal + y * bpp_internal * width] = data[2 + (width - x) * bpp + y * bpp * width];
+                data_new[1 + x * bpp_internal + y * bpp_internal * width] = data[1 + (width - x) * bpp + y * bpp * width];
+                data_new[2 + x * bpp_internal + y * bpp_internal * width] = data[0 + (width - x) * bpp + y * bpp * width];
             }
-            char data_new[width*height*bpp];
-            for(size_t x=0;x<width;x++){
-                for(size_t y=0;y<height;y++){
-                    data_new[0 + x*bpp + y*bpp*width] =  data[0 + (width-x)*bpp + y*bpp*width];
-                    data_new[1 + x*bpp + y*bpp*width] =  data[1 + (width-x)*bpp + y*bpp*width];
-                    data_new[2 + x*bpp + y*bpp*width] =  data[2 + (width-x)*bpp + y*bpp*width];
-                    data_new[3 + x*bpp + y*bpp*width] =  data[3 + (width-x)*bpp + y*bpp*width];
+        }
+        color_frame[orginal]->setImage(data_new, width * height * bpp_internal);
+        color_frame_p[orginal].reset(color_frame[orginal]);
+        if (orginal) {
+            _color_frame_orginal.write(color_frame_p[orginal]);
+        } else {
+            _color_frame.write(color_frame_p[orginal]);
+        }
+    } else {
+        LOG_ERROR_S << "Color Image: Unssuported bits per pixel size of " << bpp;
+        return false;
+    }
+    return true;
+}
+
+bool Task::onNewFrame(Frame::Type type, Frame* frame)
+{
+    switch (type) {
+        case Frame::Color:
+            if (rgb) delete rgb;
+            rgb_updated = true;
+            rgb = frame;
+            break;
+        case Frame::Ir:
+            if (ir) delete ir;
+            ir = frame;
+            break;
+        case Frame::Depth:
+            if (depth) delete depth;
+            depth = frame;
+            depth_updated = true;
+            break;
+    }
+
+    // Only process the frame if we have a depth image
+    // In this case we also write the color image (in a scaled-down version (not the orginal size))
+    if (type == Frame::Depth) {
+        if (depth && rgb && depth_updated && rgb_updated) {
+            registration->apply(rgb, depth, &undistorted, &registered);
+            size_t& width = undistorted.width;
+            size_t& height = undistorted.height;
+            size_t bpp = undistorted.bytes_per_pixel;
+            unsigned char* data = undistorted.data;
+            if (bpp == 4) {
+                if (!depth_frame) {
+                    depth_frame = new base::samples::DistanceImage(width, height);
+                    depth_frame->data.resize(width * height);
+                    Freenect2Device::IrCameraParams p = device->getIrCameraParams();
+                    depth_frame->setIntrinsic(p.fx, p.fy, p.cx, p.cy);
                 }
+                depth_frame->time = base::Time::now();
+                float* depth_data = (float*)data;
+                for (size_t x = 0; x < width; x++) {
+                    for (size_t y = 0; y < height; y++) {
+                        float value = depth_data[(width-x)+y*width] / 1000.0f;  // to meters
+                        if (isnan(value) || value <= 0.001) {
+                            value = 0.0;
+                        }
+                        depth_frame->data[x+y*width] = value;
+                    }
+                }
+                depth_frame_p.reset(depth_frame);
+                _depth_frame.write(depth_frame_p);
+            } else {
+                LOG_ERROR_S << "Depth Image: Unssuported bits per pixel size of " << bpp;
+                return false;
             }
-            ir_frame->setImage(data,width*height*bpp);
+            writeColorFrame(&registered, false);
+            rgb_updated =false;
+            depth_updated = false;
+        }
+    } else if (type == Frame::Color) {
+        // Okay we write a colored frame imidiealty in his orginal size
+        writeColorFrame(frame, true);
+    } else if (type == Frame::Ir) {
+        size_t& width = frame->width;
+        size_t& height = frame->height;
+        size_t bpp = frame->bytes_per_pixel;
+        unsigned char* data = frame->data;
+        if (bpp == 4) {
+            if (!ir_frame) {
+                ir_frame = new base::samples::frame::Frame(width, height, 32, base::samples::frame::MODE_GRAYSCALE);
+            }
+            ir_frame->setImage(data, width * height * bpp);
             ir_frame->time = base::Time::now();
             ir_frame_p.reset(ir_frame);
             _ir_frame.write(ir_frame_p);
-        }else{
+        } else {
             LOG_ERROR_S << "IR Image: Unssuported bits per pixel size of " << bpp;
-            delete frame;
             return false;
         }
-    }else if(type == Frame::Depth){
-        if(bpp == 4){
-            if(!depth_frame){
-                depth_frame = new base::samples::DistanceImage(width,height); 
-                depth_frame->data.resize(width*height);
-                Freenect2Device::IrCameraParams p = device->getIrCameraParams();
-                depth_frame->setIntrinsic(p.fx,p.fy,p.cx,p.cy);
-            }
-            depth_frame->time = base::Time::now();
-            float *depth_data = (float*)data;
-
-            for(size_t x=0;x<width;x++){
-                for(size_t y=0;y<height;y++){
-                    depth_frame->data[x+y*width] = depth_data[(width-x)+y*width]/1000.0; //to meters
-                }
-            }
-
-            depth_frame_p.reset(depth_frame);
-            _depth_frame.write(depth_frame_p);
-        }else{
-            LOG_ERROR_S << "Depth Image: Unssuported bits per pixel size of " << bpp;
-            delete frame;
-            return false;
-        }
-    }else{
+    } else {
         LOG_ERROR_S << "Unsupported Frame format";
-        delete frame;
         return false;
     }
-    delete frame;
     return true;
 }
--- tasks/Task.hpp
+++ tasks/Task.hpp
@@ -4,118 +4,133 @@
 #define KINECT2_TASK_TASK_HPP
 
 #include "kinect2/TaskBase.hpp"
+namespace libfreenect2{
+    class Registration;
+    class Frame;
+    class Freenect2Device;
+    class Freenect2;
+};
+
 #include <libfreenect2/libfreenect2.hpp>
 
-namespace kinect2 {
+namespace kinect2
+{
 
-    /*! \class Task 
-     * \brief The task context provides and requires services. It uses an ExecutionEngine to perform its functions.
-     * Essential interfaces are operations, data flow ports and properties. These interfaces have been defined using the oroGen specification.
-     * In order to modify the interfaces you should (re)use oroGen and rely on the associated workflow.
-     * 
-     * \details
-     * The name of a TaskContext is primarily defined via:
+/*! \class Task
+ * \brief The task context provides and requires services. It uses an ExecutionEngine to perform its functions.
+ * Essential interfaces are operations, data flow ports and properties. These interfaces have been defined using the oroGen specification.
+ * In order to modify the interfaces you should (re)use oroGen and rely on the associated workflow.
+ *
+ * \details
+ * The name of a TaskContext is primarily defined via:
+ \verbatim
+ deployment 'deployment_name'
+     task('custom_task_name','kinect2::Task')
+ end
+ \endverbatim
+ *  It can be dynamically adapted when the deployment is called with a prefix argument.
+ */
+class Task : public TaskBase, public libfreenect2::FrameListener
+{
+    friend class TaskBase;
+
+   protected:
+    bool writeColorFrame(libfreenect2::Frame *frame, bool orginal);
+    bool depth_updated,rgb_updated;
+    libfreenect2::Freenect2Device *device;
+    libfreenect2::Freenect2 *driver;
+    libfreenect2::Registration *registration;
+    libfreenect2::Frame undistorted, registered;
+    RTT::extras::ReadOnlyPointer<base::samples::frame::Frame> color_frame_p[2];
+    RTT::extras::ReadOnlyPointer<base::samples::frame::Frame> ir_frame_p;
+    RTT::extras::ReadOnlyPointer<base::samples::DistanceImage> depth_frame_p;
+    base::samples::frame::Frame *color_frame[2];
+    base::samples::frame::Frame *ir_frame;
+    base::samples::DistanceImage *depth_frame;
+
+    libfreenect2::Frame *rgb;
+    libfreenect2::Frame *ir;
+    libfreenect2::Frame *depth;
+
+   public:
+    /** TaskContext constructor for Task
+     * \param name Name of the task. This name needs to be unique to make it identifiable via nameservices.
+     * \param initial_state The initial TaskState of the TaskContext. Default is Stopped state.
+     */
+    Task(std::string const &name = "kinect2::Task");
+
+    /** TaskContext constructor for Task
+     * \param name Name of the task. This name needs to be unique to make it identifiable for nameservices.
+     * \param engine The RTT Execution engine to be used for this task, which serialises the execution of all commands, programs, state machines and incoming events for a task.
+     *
+     */
+    Task(std::string const &name, RTT::ExecutionEngine *engine);
+
+    /** Default deconstructor of Task
+     */
+    ~Task();
+
+    /** This hook is called by Orocos when the state machine transitions
+     * from PreOperational to Stopped. If it returns false, then the
+     * component will stay in PreOperational. Otherwise, it goes into
+     * Stopped.
+     *
+     * It is meaningful only if the #needs_configuration has been specified
+     * in the task context definition with (for example):
      \verbatim
-     deployment 'deployment_name'
-         task('custom_task_name','kinect2::Task')
+     task_context "TaskName" do
+       needs_configuration
+       ...
      end
      \endverbatim
-     *  It can be dynamically adapted when the deployment is called with a prefix argument. 
      */
-    class Task : public TaskBase, public libfreenect2::FrameListener
-    {
-	friend class TaskBase;
-    protected:
-        libfreenect2::Freenect2Device *device;
-        libfreenect2::Freenect2 *driver;
-        RTT::extras::ReadOnlyPointer<base::samples::frame::Frame> color_frame_p;
-        RTT::extras::ReadOnlyPointer<base::samples::frame::Frame> ir_frame_p;
-        RTT::extras::ReadOnlyPointer<base::samples::DistanceImage> depth_frame_p;
-        base::samples::frame::Frame *color_frame;
-        base::samples::frame::Frame *ir_frame;
-        base::samples::DistanceImage *depth_frame;
+    bool configureHook();
 
+    /** This hook is called by Orocos when the state machine transitions
+     * from Stopped to Running. If it returns false, then the component will
+     * stay in Stopped. Otherwise, it goes into Running and updateHook()
+     * will be called.
+     */
+    bool startHook();
 
-    public:
-        /** TaskContext constructor for Task
-         * \param name Name of the task. This name needs to be unique to make it identifiable via nameservices.
-         * \param initial_state The initial TaskState of the TaskContext. Default is Stopped state.
-         */
-        Task(std::string const& name = "kinect2::Task");
+    /** This hook is called by Orocos when the component is in the Running
+     * state, at each activity step. Here, the activity gives the "ticks"
+     * when the hook should be called.
+     *
+     * The error(), exception() and fatal() calls, when called in this hook,
+     * allow to get into the associated RunTimeError, Exception and
+     * FatalError states.
+     *
+     * In the first case, updateHook() is still called, and recover() allows
+     * you to go back into the Running state.  In the second case, the
+     * errorHook() will be called instead of updateHook(). In Exception, the
+     * component is stopped and recover() needs to be called before starting
+     * it again. Finally, FatalError cannot be recovered.
+     */
+    void updateHook();
 
-        /** TaskContext constructor for Task 
-         * \param name Name of the task. This name needs to be unique to make it identifiable for nameservices. 
-         * \param engine The RTT Execution engine to be used for this task, which serialises the execution of all commands, programs, state machines and incoming events for a task. 
-         * 
-         */
-        Task(std::string const& name, RTT::ExecutionEngine* engine);
+    /** This hook is called by Orocos when the component is in the
+     * RunTimeError state, at each activity step. See the discussion in
+     * updateHook() about triggering options.
+     *
+     * Call recover() to go back in the Runtime state.
+     */
+    void errorHook();
 
-        /** Default deconstructor of Task
-         */
-	~Task();
+    /** This hook is called by Orocos when the state machine transitions
+     * from Running to Stopped after stop() has been called.
+     */
+    void stopHook();
 
-        /** This hook is called by Orocos when the state machine transitions
-         * from PreOperational to Stopped. If it returns false, then the
-         * component will stay in PreOperational. Otherwise, it goes into
-         * Stopped.
-         *
-         * It is meaningful only if the #needs_configuration has been specified
-         * in the task context definition with (for example):
-         \verbatim
-         task_context "TaskName" do
-           needs_configuration
-           ...
-         end
-         \endverbatim
-         */
-        bool configureHook();
+    /** This hook is called by Orocos when the state machine transitions
+     * from Stopped to PreOperational, requiring the call to configureHook()
+     * before calling start() again.
+     */
+    void cleanupHook();
 
-        /** This hook is called by Orocos when the state machine transitions
-         * from Stopped to Running. If it returns false, then the component will
-         * stay in Stopped. Otherwise, it goes into Running and updateHook()
-         * will be called.
-         */
-        bool startHook();
-
-        /** This hook is called by Orocos when the component is in the Running
-         * state, at each activity step. Here, the activity gives the "ticks"
-         * when the hook should be called.
-         *
-         * The error(), exception() and fatal() calls, when called in this hook,
-         * allow to get into the associated RunTimeError, Exception and
-         * FatalError states. 
-         *
-         * In the first case, updateHook() is still called, and recover() allows
-         * you to go back into the Running state.  In the second case, the
-         * errorHook() will be called instead of updateHook(). In Exception, the
-         * component is stopped and recover() needs to be called before starting
-         * it again. Finally, FatalError cannot be recovered.
-         */
-        void updateHook();
-
-        /** This hook is called by Orocos when the component is in the
-         * RunTimeError state, at each activity step. See the discussion in
-         * updateHook() about triggering options.
-         *
-         * Call recover() to go back in the Runtime state.
-         */
-        void errorHook();
-
-        /** This hook is called by Orocos when the state machine transitions
-         * from Running to Stopped after stop() has been called.
-         */
-        void stopHook();
-
-        /** This hook is called by Orocos when the state machine transitions
-         * from Stopped to PreOperational, requiring the call to configureHook()
-         * before calling start() again.
-         */
-        void cleanupHook();
-
-        // Callback for reciving new frames from FrameListener
-        virtual bool onNewFrame(libfreenect2::Frame::Type type, libfreenect2::Frame *frame);
-    };
+    // Callback for reciving new frames from FrameListener
+    virtual bool onNewFrame(libfreenect2::Frame::Type type, libfreenect2::Frame *frame);
+};
 }
 
 #endif
-
